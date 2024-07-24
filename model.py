df3df8afaf9a0f2a79753d219812e4332132236d
Devansh Lingamaneni <115863065+DevanshL@users.noreply.github.com>
2024-07-24 22:18:13 +0530

    Update model.py

--- model.py
+++ model.py
@@ -5,7 +5,9 @@ import json
 def download_bar(curr,total,width=100):
   print("Downloading %d%% {%d / %d} bytes" % (curr / total * 100, curr, total))
 
-model_url = "https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/resolve/main/llama-2-7b-chat.Q8_0.gguf"
-config = json.load(open('config.json'))
-hf_token = config['HF_TOKEN']
-model_path = wget.download(model_url, bar=download_bar)
+def download_model():
+    model_url = "https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGUF/resolve/main/llama-2-7b-chat.Q8_0.gguf"
+    config = json.load(open('config.json'))
+    hf_token = config['HF_TOKEN']
+    model_path = wget.download(model_url, bar=download_bar)
+    return model_path
